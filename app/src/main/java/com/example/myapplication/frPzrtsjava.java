9c7fba02cf04f7e87ebb1c4d3eb321a79d8a6ef3
talhacomak <tcomak32@outlook.com>
2021-01-29 12:04:56 +0300

    some updates

--- app/src/main/java/com/example/myapplication/frPzrtsjava.java
+++ app/src/main/java/com/example/myapplication/frPzrtsjava.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class frPzrtsjava extends Fragment  {
